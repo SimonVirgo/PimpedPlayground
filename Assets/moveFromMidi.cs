621aceb72535dd683f305c8dc4f8d09cc4f243b6
SimonVirgo <s.virgo@ged.rwth-aachen.de>
2022-05-29 19:41:43 +0200

    add assets from BOA setup

---
+++ Assets/moveFromMidi.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering.HighDefinition.Attributes;
+
+public class moveFromMidi : MonoBehaviour
+{
+    public VfxMidi Midi; 
+    public float minDistanceZ;
+
+    public float maxDistanceZ;
+    public float minDistanceY;
+
+    public float maxDistanceY;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Midi = new VfxMidi();
+        //Midi.camera.cameraZ.started += ctx => move(ctx.ReadValue<float>());
+    }
+
+
+    void OnEnable()
+    {
+        Midi.Enable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var valueZ = Midi.camera.cameraZ.ReadValue<float>();
+        var valueY = Midi.camera.cameraY.ReadValue<float>();
+        var mappedZ = Mathf.Lerp(minDistanceZ, maxDistanceZ, valueZ);
+        var mappedY = Mathf.Lerp(minDistanceY, maxDistanceY, valueY);
+        var x = transform.localPosition.x;
+        var y = mappedY;
+        var z = mappedZ;
+        
+        transform.localPosition = new Vector3(x,y,z);
+    }
+}
